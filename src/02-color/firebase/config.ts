23dc5c3f2f089130415a7db9e6b10eb6d5199103
kenji <k.saito.1985@gmail.com>
2019-11-05 23:17:15 +0900

    update

---
+++ src/02-color/firebase/config.ts
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+	apiKey: 'AIzaSyDSlTdL6ujGfA5sHGtPElv86ROFby-zf04',
+	authDomain: 'creative-coding-sketches.firebaseapp.com',
+	databaseURL: 'https://creative-coding-sketches.firebaseio.com',
+	projectId: 'creative-coding-sketches',
+	storageBucket: 'creative-coding-sketches.appspot.com',
+	messagingSenderId: '918212412691',
+	appId: '1:918212412691:web:7b8412eeb19d4f0f57c6e6',
+	measurementId: 'G-EXWQF2GK7P'
+};
