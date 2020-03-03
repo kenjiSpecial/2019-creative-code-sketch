b5990e67ca659ab2a5a75d7033bfb59c89791fcb
kenji <k.saito.1985@gmail.com>
2020-03-03 09:36:11 +0900

    update

---
+++ src/09-machine-learning/index.js
@@ -0,0 +1,8 @@
+import * as THREE from 'three';
+const OrbitControls = require('three-orbit-controls')(THREE);
+import { gsap } from 'gsap';
+import dat from 'dat.gui';
+
+function init(){}
+
+init();
